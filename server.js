812005c64f0fdf4fca15e2e220aa0d1e35c28c6b
Tharindu <nbandara396@gmail.com>
2025-09-06 12:03:15 +0530

    updated

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors({
   origin: ["https://frontend-eight-ivory.vercel.app",
     "http://localhost:3000",
-    "http://localhost:5173/"
+    "http://localhost:5173"
   ],
   methods:["POST","GET","UPDATE","DELETE"],
   credentials:true
